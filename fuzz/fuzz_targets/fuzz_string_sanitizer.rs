61cf66d0faf105f922aa99e43707aff7ecab11d8
Jordan Petridis <jordanpetridis@protonmail.com>
2017-10-08 21:05:51 +0300

    Updated fuzzers.

--- fuzz/fuzz_targets/fuzz_string_sanitizer.rs
+++ fuzz/fuzz_targets/fuzz_string_sanitizer.rs
@@ -4,8 +4,7 @@ extern crate libfuzzer_sys;
 extern crate rfc822_sanitizer;
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    if let Ok(utf8) = std::str::from_utf8(data) {
+    if let Ok(utf8) = String::from_utf8(data.to_vec()) {
         let _ = rfc822_sanitizer::sanitize_rfc822_like_date(utf8);
     }
 });
--- fuzz/fuzz_targets/fuzz_with_fallback.rs
+++ fuzz/fuzz_targets/fuzz_with_fallback.rs
@@ -4,7 +4,7 @@ extern crate libfuzzer_sys;
 extern crate rfc822_sanitizer;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(utf8) = std::str::from_utf8(data) {
+    if let Ok(utf8) = String::from_utf8(data.to_vec()) {
         let _ = rfc822_sanitizer::parse_from_rfc2822_with_fallback(utf8);
     }
 });
